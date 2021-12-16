ab319a4811d1987d96dc1e2635507d64e193cae7
figlerr <figler.renata@tmx.hu>
2021-12-16 16:15:47 +0100

    forras

---
+++ src/main/java/hu/tmx/personJava/App.java
@@ -0,0 +1,13 @@
+package hu.tmx.personJava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
